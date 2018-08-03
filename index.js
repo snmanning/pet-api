c13470d7275db79a64b840a6de3858515d425982
Sarah Manning <sarahnmanning@gmail.com>
2018-08-02 20:34:48 -0400

    simple pet model using mongoose and connected to the database. Implemented the basic get all route

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const server = express();
+const mongoose = require('mongoose');
 
 // set up environment variables
 dotenv.config();
+
+// connect to the database
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true });
 
 // set up the port
 const port = process.env.PORT || 8008;
 
 // power ups (middleware)
 
+// models
+const Pet = mongoose.model('Pet', { name : String, owner: String });
 
 // routes
 // get all pets
-server.get('/pets', (req, res) => {
-    res.send('getting all pets');
+server.get('/pets', async (req, res) => {
+    try {
+        const pets = await Pet.find();
+        res.status(200).json({
+            "pets": pets
+        })
+    } catch(err) {
+        res.status(500).jason({
+            msg: "stuff done broke"
+        });
+    }
 });
 // get one special pet by id
 server.get('/pets/:id', (req, res) => {
